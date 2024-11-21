db69b3efe5667f45c8d8d6828d200e587a5499f8
Maria Clara <maria.clara@bcb.gov.br>
2024-11-21 17:42:07 -0300

    excluindo item

--- src/controller/itemController.js
+++ src/controller/itemController.js
@@ -142,9 +142,9 @@ function excluirItem(req, res) {
         }
     }).then((item)=>{
         item.destroy();
-        res.redirect('/lista_itens/' + req.params.idEstabelecimento);
+        res.redirect('/homeRestaurante');
     }).catch((erro_recupera_itens)=>{
-        res.render('listaItens.html', {erro_recupera_itens});
+        res.render('homeRestaurante.html', {erro_recupera_itens});
     }); 
 }
 module.exports = {
